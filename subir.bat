6961c07c24b41c0bb23febbaedc0ea759b81602a
carlosherrerah <carlos.herrera@upa.edu.mx>
2025-01-15 10:59:10 -0600

    Actualizacion automatica - Get-Date : No se puede enlazar el parámetro 'Date'. No se puede convertir el valor HH:mm:ss al tipo

--- subir.bat
+++ subir.bat
@@ -2,7 +2,8 @@ git status
 timeout /t 3
 git add .
 timeout /t 3
-git commit -m "Actualizacion automatica - $(date +'%Y-%m-%d %H:%M:%S')"
+rem git commit -m "Actualizacion automatica - $(date +'%Y-%m-%d %H:%M:%S')"
+for /f "tokens=*" %%A in ('powershell -command "Get-Date -Format yyyy-MM-dd HH:mm:ss"') do git commit -m "Actualizacion automatica - %%A"
 timeout /t 10
 git push
 
