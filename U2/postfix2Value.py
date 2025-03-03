ca7d6294e71d7f93657fb418e25065772389d26a
= <=>
2025-03-02 18:36:56 -0600

    Automatico

--- U2/postfix2Value.py
+++ U2/postfix2Value.py
@@ -7,6 +7,12 @@ def postfix2Value(postfix):
     value = 10      
     return value
 
+
+infix = '5 * ( 6 + 2 ) - 12 / 4'	
+posfix = '5 6 2 + * 12 4 / -'
+Valor = 37
+
+
 infix = "4-3^2/3+7*(1-2)"
 postfix = "4 3 2 ^ 3 / - 7 1 2 - * +"
 valor = -6
@@ -14,6 +20,10 @@ valor = -6
 infix  = '2*(2^3*2-6/(4-2)-10)-2' 
 postfix = "2 2 3 ^ 2 * 6 4 2 - / - 10 - * 2 -"
 valor = 4
+
+infix = '2 ^ 4 / ( 4 * 1 ) + log ( 110 - 10 ) ^ 2'  
+posfix = '2 4 ^ 4 1 * / 110 10 - log 2 ^ +'
+valor = 8
 
 valor = postfix2Value(postfix)
 print(valor)
